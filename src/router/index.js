b7a4f4343e801e7f4f1910e7c7b2a4070c5c1c96
AuthorJim <1036760040@qq.com>
2017-07-25 18:41:07 +0800

    mission complete

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ import Publish from '../pages/detailPage/detailData/publish.vue'
 Vue.use(Router)
 
 export default new Router({
-	mode: 'history',
+	// mode: 'history',
   routes: [
   {
   path: '/',
